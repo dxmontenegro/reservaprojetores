82e4f3cad7632256dcdf363fd2eb3019a3b71c99
dxmontenegro <monte.eng@gmail.com>
2026-04-21 18:38:20 -0300

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,27 +1,21 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
-);
-
 export default async function handler(req, res) {
-
-  res.setHeader("Access-Control-Allow-Origin", "*");
 
   try {
 
-    // 🔥 SÓ TROQUE AQUI PELO NOME DA SUA TABELA
-    const { error } = await supabase
-      .from('reserva_recursos') // 👈 coloque sua tabela aqui
-      .select('*')
-      .limit(1);
+    const resposta = await fetch(
+      "https://bdjqajzhlkdfagdfknmh.supabase.co/rest/v1/reservas_recursos?select=*&limit=1",
+      {
+        headers: {
+          apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkanFhanpobGtkZmFnZGZrbm1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NTA5MDcsImV4cCI6MjA4NjMyNjkwN30.hxh4adeSNM7FazeNkinwPrJEZdxsZkhtPTB5zU6eub0"
+        }
+      }
+    );
 
-    if (error) throw error;
+    if (!resposta.ok) throw new Error("erro");
 
-    res.status(200).json({ status: "ok", supabase: "ativo" });
+    res.status(200).json({ status: "ok" });
 
   } catch (e) {
-    res.status(500).json({ status: "erro", detalhe: e.message });
+    res.status(500).json({ status: "erro" });
   }
 }
